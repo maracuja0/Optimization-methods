bded98f9e0d38ef6c45d27475b77b4b499b3e160
maracuja0 <110384417+maracuja0@users.noreply.github.com>
2024-03-23 22:56:48 +0400

    - Немного оптимизировал метод дихотомии - Сделал более красивый вывод в консоль с флагом _DEBUG.

--- src/OneDimensional.cpp
+++ src/OneDimensional.cpp
@@ -4,28 +4,32 @@
 
 
 #include "../include/OneDimensional.h"
-#include <cmath>
 #include <algorithm>
 #include <iostream>
 
 namespace oneDimensional{
     double bisect(function_1d func, double x_0, double x_1, const int max_iters, const double eps)
     {
-        double x_l = x_0, x_r = x_1, x_c = 0.0;
+        double x_c = 0.0;
 
-        if (x_l > x_r) std::swap(x_l, x_r);
+        if (x_0 > x_1) std::swap(x_0, x_1);
 
         int cnt = 0;
         while (cnt != max_iters) {
 
-            if (std::abs(x_r - x_l) < eps) break;
+            if (std::abs(x_1 - x_0) < 2 * eps) break;
             cnt++;
 
-            x_c = (x_l + x_r) * 0.5;
-            (func(x_c - eps) > func(x_c + eps)) ? x_l = x_c : x_r = x_c;
+            x_c = (x_0 + x_1) * 0.5;
+            if (func(x_c - eps) < func(x_c + eps)){
+                x_1 = x_c;
+            }else{
+                x_0 = x_c;
+            }
         }
-#if DEBUG
-        std::cout << "\ndihotomia iterations number : " << cnt << "\n";
+#if _DEBUG
+        std::cout << "\nIterations number : " << cnt << "\n";
+        std::cout << "Answer: ";
 #endif
         return x_c;
     }
@@ -40,17 +44,22 @@ namespace oneDimensional{
 
         int cnt = 0;
         while (cnt != max_iters) {
-            if (x_r - x_l < eps) break;
+            if (std::abs(x_r - x_l) < eps) break;
             cnt++;
 
             dx = b - a;
             x_l = b - dx * ONE_OVER_PHI;
             x_r = a + dx * ONE_OVER_PHI;
 
-            (func(x_l) >= func(x_r)) ? a = x_l : b = x_r;
+            if (func(x_l) >= func(x_r)){
+                a = x_l;
+            }else{
+                b = x_r;
+            }
         }
-#if DEBUG
-        std::cout << "\ngolden ratio iterations number : " << cnt << "\n";
+#if _DEBUG
+        std::cout << "\nIterations number : " << cnt << "\n";
+        std::cout << "Answer: ";
 #endif
         return (x_r + x_l) * 0.5;
     }
@@ -97,8 +106,9 @@ namespace oneDimensional{
 
             (func(x_l) < func(x_r)) ? b = x_r : a = x_l;
         }
-#if DEBUG
-        std::cout << "\nfibonacchi iterations number : " << cnt << "\n";
+#if _DEBUG
+        std::cout << "\nIterations number : " << cnt << "\n";
+        std::cout << "Answer: ";
 #endif
         return (x_l + x_r) * 0.5;
     }
--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,21 @@ double f(const double x);
 void calcPHI(){
     double fi =((1 + pow(5, 0.5)) / 2);
 
-    std::cout << "PHI: " << std::setprecision(20)<<  fi << " ONE_OVER_PHI: " << 1 / fi << '\n';
+    std::cout << "PHI: " << std::setprecision(20)<<  fi << ", ONE_OVER_PHI: " << 1 / fi << '\n';
 }
+
 void LR1(){
     std::cout << "\n---------------\n";
     std::cout << "------LR1------\n";
     std::cout << "---------------\n\n";
+    std::cout << "One dimensional search:\n\n";
 
-    int x_0 = 10, x_1 = 1;
-    std::cout << "Function: (x-5)*x\n";
-    std::cout << "x_0: " << x_0 << ", x_1: " << x_1 << "\n";
+    double x_0 = -2, x_1 = 10;
+    std::cout << "y = (x - 5) * x\n" << "x_0 = " << x_0 << ", x_1 = " << x_1 << "\n\n";
 
-    std::cout << "Bisect: " << oneDimensional::bisect(f, x_0, x_1) << "\n";
-    std::cout << "Golden ration: " << oneDimensional::golden_ratio(f, x_0, x_1) << "\n";
-    std::cout << "Fibonacci:" << oneDimensional::fibonacci(f, x_0, x_1) << "\n";
+    std::cout << "Bisect: " << oneDimensional::bisect(f, x_0, x_1) << "\n\n";
+    std::cout << "Golden ratio: " << oneDimensional::golden_ratio(f, x_0, x_1) << "\n\n";
+    std::cout << "Fibonacci: " << oneDimensional::fibonacci(f, x_0, x_1) << "\n\n";
 }
 
 void LR2(){
